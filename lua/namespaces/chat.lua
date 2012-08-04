3a0ab2f8328125f8bf777f542d6c5a0395601ac1
Adam Burton <adam@burt0n.net>
2012-08-04 12:22:53 +0100

    Fixed chat (find a better way of doing this)

--- lua/namespaces/chat.lua
+++ lua/namespaces/chat.lua
@@ -3,7 +3,7 @@ NS.dependencies = { 'core' }
 NS.chats = {}
 
 function NS.GetLatestChats(limit)
-	return table.slice(self.chats, #self.chats, -limit)
+	return api.namespaces['chat'].chats
 end
 
 function NS.AddChat(player, text, public)
@@ -14,7 +14,7 @@ end
 --------------------------------------
 
 hook.Add('PlayerSay', 'JSONAPI.Chat.PlayerSay', function(ply, text, public)
-	local ply = api.Call('core', 'getPlayer', { uniqueid = ply:UniqueID() }, true)
+	local ply = api.Call('player', 'GetPlayer', { uniqueid = ply:UniqueID() }, true)
 	
-	table.insert(NS.chats, {player = ply, text = text, public = public, time = os.time()})
+	table.insert(api.namespaces['chat'].chats, {player = ply, text = text, public = public, time = os.time()})
 end)
